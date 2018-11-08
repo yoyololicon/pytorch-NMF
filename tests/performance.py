43b455613db4264f0e2b1eb5e54931dc1016c210
ycy <ya70201@gmail.com>
2018-11-08 23:25:30 +0800

    change time scale in performance test to log.

--- tests/performance.py
+++ tests/performance.py
@@ -8,52 +8,56 @@ from torchnmf.models import NMF as torchNMF
 from sklearn.decomposition import NMF
 from time import time
 
-# torch.set_flush_denormal(True)
-#torch.set_default_tensor_type(torch.DoubleTensor)
+torch.set_flush_denormal(True)
+# torch.set_default_tensor_type(torch.DoubleTensor)
 
 if __name__ == '__main__':
-    y, sr = load(
-        '/media/ycy/Shared/Datasets/DSD100subset/Sources/Test/005 - Angela Thomas Wade - Milk Cow Blues/drums.wav',
-        normalization=True)
-    windowsize = 2048
-    S = torch.stft(y.mean(1), windowsize, window=torch.hann_window(windowsize)).pow(2).sum(2).sqrt()
-    R = 4
-    max_iter = 80
+    duration = 60
+    y, sr = load('/media/ycy/Shared/Datasets/MAPS/ENSTDkCl/MUS/MAPS_MUS-alb_se2_ENSTDkCl.wav', normalization=True)
+    y = y.mean(1)[:duration * sr]
+    windowsize = 4096
+    S = torch.stft(y, windowsize, window=torch.hann_window(windowsize)).pow(2).sum(2).sqrt()
+    R = 88
+    max_iter = 50
 
     betas = [0, 0.5, 1, 1.5, 2]
     sk = []
     tch = []
     tchcuda = []
 
+    Snumpy = S.numpy()
+    Scuda = S.cuda()
+
     for b in betas:
         print('beta =', b)
         model = NMF(R, 'random', solver='mu', max_iter=max_iter, beta_loss=b, verbose=True)
         start = time()
-        model.fit(S.numpy())
-        rate = model.n_iter_ / (time() - start)
+        model.fit(Snumpy)
+        rate = (time() - start) / model.n_iter_
         print('sklearn', rate)
         sk.append(rate)
 
         net = torchNMF(S.shape, n_components=R, max_iter=max_iter, beta_loss=b, verbose=True)
         start = time()
         niter = net.fit(S)
-        rate = niter / (time() - start)
+        rate = (time() - start) /niter
         print('torch', rate)
         tch.append(rate)
 
-        S2 = S.cuda()
         net = torchNMF(S.shape, n_components=R, max_iter=max_iter, beta_loss=b, verbose=True).cuda()
         start = time()
-        niter = net.fit(S2)
-        rate = niter / (time() - start)
-        print('torch+cuda', rate)
+        niter = net.fit(Scuda)
+        rate = (time() - start) /niter
+        print('torch + cuda', rate)
         tchcuda.append(rate)
 
-    plt.bar(np.array(betas) - 0.1, sk, width=0.1, align='center', label='sklearn')
-    plt.bar(betas , tch, width=0.1, align='center', label='torch')
-    plt.bar(np.array(betas) + 0.1, tchcuda, width=0.1, align='center', label='torch+cuda')
+    plt.bar(np.array(betas) - 0.15, sk, width=0.15, align='center', label='sklearn')
+    plt.bar(betas, tch, width=0.15, align='center', label='torch')
+    plt.bar(np.array(betas) + 0.15, tchcuda, width=0.15, align='center', label='torch+cuda')
     plt.xlabel(r'$\beta$')
-    plt.ylabel("iterations/s")
+    plt.ylabel("Time per Iteration (s)")
+    plt.yscale('log')
     plt.legend()
+    plt.title('Runtime Benchmark, target matrix size is %d x %d, %d components' % (S.shape[0], S.shape[1], R))
     plt.xticks(betas, [str(i) for i in betas])
     plt.show()
