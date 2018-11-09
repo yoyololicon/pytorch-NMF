6d80fc64f03cbfb93aa9e7245e964bfa70394dbe
ycy <ya70201@gmail.com>
2018-11-09 12:27:51 +0800

    change version to v0.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yoyololicon/pytorch-NMFs",
     packages=setuptools.find_packages(),
-    install_requires=['numpy', 'torch'],
+    install_requires=['torch==0.4.1'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
--- torchnmf/__init__.py
+++ torchnmf/__init__.py
@@ -1,8 +1,8 @@
 # nothing
 
 name = "torchnmf"
-__version__ = '0.1'
+__version__ = '0.1.1'
 __maintainer__ = 'Chin-Yun Yu'
 __email__ = 'lolimaster.cs03@nctu.edu.tw'
 
-from .models import *
+from .models import NMF, NMFD
