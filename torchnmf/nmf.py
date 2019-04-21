1236726ffb4e4401a748a3e5affb4260ff7de2a4
ycy <ya70201@gmail.com>
2019-04-22 00:03:38 +0800

    update nmf class structure.

--- torchnmf/nmf.py
+++ torchnmf/nmf.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch import nn
+from torch.nn import Parameter
 from math import sqrt
 from .metrics import Beta_divergence
 from .base import Base
@@ -9,46 +9,146 @@ from tqdm import tqdm
 import numpy as np
 
 
-def _2sqrt_error(x):
-    return sqrt(x * 2)
+def _mu_update(param, pos, gamma, l1_reg, l2_reg):
+    if param.grad is None:
+        return
+    multiplier = pos - param.grad
+    if l1_reg > 0:
+        pos.add_(l1_reg)
+    if l2_reg > 0:
+        if pos.shape != param.data.shape:
+            pos = pos + l2_reg * param
+        else:
+            pos.add_(l2_reg * param)
+    multiplier.div_(pos)
+    if gamma != 1:
+        multiplier.pow_(gamma)
+    param.mul_(multiplier)
 
 
 class NMF_(Base):
-    def __init__(self):
+    def __init__(self, W_size, H_size, rank):
         super().__init__()
-
-    def get_W_positive(self, *args):
-        raise NotImplementedError
-
-    def get_H_positive(self, *args):
-        raise NotImplementedError
-
-
-class NMF(nn.Module):
-
-    def __init__(self, Xshape, rank=None):
-        super().__init__()
-        self.K, self.M = Xshape
-        if not rank:
-            self.rank = self.K
-        else:
-            self.rank = rank
-
-        self.W = torch.nn.Parameter(torch.rand(self.K, self.rank))
-        self.H = torch.nn.Parameter(torch.rand(self.rank, self.M))
+        self.rank = rank
+        self.W = Parameter(torch.rand(*W_size))
+        self.H = Parameter(torch.rand(*H_size))
 
     def forward(self, H=None, W=None):
         if H is None:
             H = self.H
         if W is None:
             W = self.W
+        return self.reconstruct(H, W)
+
+    def reconstruct(self, H, W):
+        raise NotImplementedError
+
+    def get_W_positive(self, WH, beta, H_sum) -> (torch.Tensor, None or torch.Tensor):
+        raise NotImplementedError
+
+    def get_H_positive(self, WH, beta, W_sum) -> (torch.Tensor, None or torch.Tensor):
+        raise NotImplementedError
+
+    def fit(self,
+            V,
+            W=None,
+            H=None,
+            update_W=True,
+            update_H=True,
+            beta=1,
+            tol=1e-5,
+            max_iter=200,
+            verbose=0,
+            initial='random',
+            alpha=0,
+            l1_ratio=0
+            ):
+        if W is None:
+            pass  # will do special initialization in thre future
+        else:
+            self.W.data.copy_(W)
+            self.W.requires_grad = update_W
+
+        if H is None:
+            pass
+        else:
+            self.H.data.copy_(H)
+            self.H.requires_grad = update_H
+
+        if beta < 1:
+            gamma = 1 / (2 - beta)
+        elif beta > 2:
+            gamma = 1 / (beta - 1)
+        else:
+            gamma = 1
+
+        l1_reg = alpha * l1_ratio
+        l2_reg = alpha * (1 - l1_ratio)
+
+        WH = self.forward()
+        loss_scale = torch.prod(torch.tensor(V.shape)).float()
+        loss = Beta_divergence(WH, V, beta) / loss_scale
+        previous_loss = loss_init = loss
+
+        H_sum, W_sum = None, None
+        with tqdm(total=max_iter) as pbar:
+            for n_iter in range(1, max_iter + 1):
+                if self.W.requires_grad:
+                    self.zero_grad()
+                    WH = self.reconstruct(self.H.detach(), self.W)
+                    loss = Beta_divergence(V, WH, beta)
+                    loss.backward()
+
+                    with torch.no_grad():
+                        positive_comps, H_sum = self.get_W_positive(WH, beta, H_sum)
+                        _mu_update(self.W, positive_comps, gamma, l1_reg, l2_reg)
+                    W_sum = None
+
+                if self.H.requires_grad:
+                    self.zero_grad()
+                    WH = self.reconstruct(self.H, self.W.detach())
+                    loss = Beta_divergence(WH, V, beta)
+                    loss.backward()
+
+                    with torch.no_grad():
+                        positive_comps, W_sum = self.get_H_positive(V, beta, W_sum)
+                        _mu_update(self.H, positive_comps, gamma, l1_reg, l2_reg)
+                    H_sum = None
+
+                loss = loss.item() / loss_scale
+                if verbose:
+                    pbar.set_postfix(loss=loss)
+                    # pbar.set_description('Beta loss=%.4f' % error)
+                    pbar.update()
+
+                if (previous_loss - loss) / loss_init < tol:
+                    break
+                previous_loss = loss
+
+        return n_iter
+
+    def fit_transform(self, *args, **kwargs):
+        n_iter = self.fit(*args, **kwargs)
+        return n_iter, self.forward()
+
+
+class NMF(NMF_):
+
+    def __init__(self, Xshape, rank=None):
+        self.K, self.M = Xshape
+        if not rank:
+            rank = self.K
+
+        super().__init__((self.K, rank), (rank, self.M), rank)
+
+    def reconstruct(self, H, W):
         return W @ H
 
-    def _get_W_positive(self, WH, beta, H_sum=None):
+    def get_W_positive(self, WH, beta, H_sum):
         H = self.H
         if beta == 1:
             if H_sum is None:
-                H_sum = H.sum(1)  # shape(n_components, )
+                H_sum = H.sum(1)
             denominator = H_sum[None, :]
         else:
             if beta != 2:
@@ -58,7 +158,7 @@ class NMF(nn.Module):
 
         return denominator, H_sum
 
-    def _get_H_positive(self, WH, beta, W_sum=None):
+    def get_H_positive(self, WH, beta, W_sum):
         W = self.W
         if beta == 1:
             if W_sum is None:
@@ -71,110 +171,6 @@ class NMF(nn.Module):
             denominator = WtWH
         return denominator, W_sum
 
-    def _mu_update(self, param, pos, gamma, l1_reg, l2_reg):
-        if param.grad is None:
-            return
-        multiplier = pos - param.grad
-        if l1_reg > 0:
-            pos.add_(l1_reg)
-        if l2_reg > 0:
-            if pos.shape != param.data.shape:
-                pos = pos + l2_reg * param
-            else:
-                pos.add_(l2_reg * param)
-        multiplier.div_(pos)
-        if gamma != 1:
-            multiplier.pow_(gamma)
-        param.mul_(multiplier)
-
-    def _2sqrt_error(self, x):
-        return sqrt(x * 2)
-
-    def fit(self,
-            X,
-            W=None,
-            H=None,
-            update_W=True,
-            update_H=True,
-            beta_loss='frobenius',
-            tol=1e-5,
-            max_iter=200,
-            verbose=0,
-            initial='random',
-            alpha=0,
-            l1_ratio=0
-            ):
-        if W is None:
-            if initial == 'random':
-                self._random_weight(self.W, X.mean())
-        else:
-            self.W.data.copy_(W)
-            self.W.requires_grad = update_W
-
-        if H is None:
-            if initial == 'random':
-                self._random_weight(self.H, X.mean())
-        else:
-            self.H.data.copy_(H)
-            self.H.requires_grad = update_H
-        beta = _beta_loss_to_float(beta_loss)
-
-        if beta < 1:
-            gamma = 1 / (2 - beta)
-        elif beta > 2:
-            gamma = 1 / (beta - 1)
-        else:
-            gamma = 1
-
-        l1_reg = alpha * l1_ratio
-        l2_reg = alpha * (1 - l1_ratio)
-
-        V = self.forward()
-        loss = Beta_divergence(V, X, beta)
-        error = self._2sqrt_error(loss.item())
-        previous_error = error_at_init = error
-
-        H_sum, W_sum = None, None
-        with tqdm(total=max_iter) as pbar:
-            for n_iter in range(1, max_iter + 1):
-                if self.W.requires_grad:
-                    self.zero_grad()
-                    V = self.forward(H=self.H.detach())
-                    loss = Beta_divergence(V, X, beta)
-                    loss.backward()
-
-                    with torch.no_grad():
-                        positive_comps, H_sum = self._get_W_positive(V, beta, H_sum)
-                        self._mu_update(self.W, positive_comps, gamma, l1_reg, l2_reg)
-                    W_sum = None
-
-                if self.H.requires_grad:
-                    self.zero_grad()
-                    V = self.forward(W=self.W.detach())
-                    loss = Beta_divergence(V, X, beta)
-                    loss.backward()
-
-                    with torch.no_grad():
-                        positive_comps, W_sum = self._get_H_positive(V, beta, W_sum)
-                        self._mu_update(self.H, positive_comps, gamma, l1_reg, l2_reg)
-                    H_sum = None
-
-                error = self._2sqrt_error(loss.item())
-                if verbose:
-                    # pbar.set_postfix(loss=error)
-                    pbar.set_description('Beta loss=%.4f' % error)
-                    pbar.update()
-
-                if (previous_error - error) / error_at_init < tol:
-                    break
-                previous_error = error
-
-        return n_iter
-
-    def fit_transform(self, *args, **kwargs):
-        n_iter = self.fit(*args, **kwargs)
-        return n_iter, self.forward(self.H)
-
     def sort(self):
         _, maxidx = self.W.data.max(0)
         _, idx = maxidx.sort()
@@ -182,34 +178,18 @@ class NMF(nn.Module):
         self.H.data = self.H.data[idx]
 
 
-class NMFD(NMF):
-    """
-    NMF deconvolution model.
+class NMFD(NMF_):
+    def __init__(self, Xshape, T=1, rank=None):
+        self.K, self.M = Xshape
+        if not rank:
+            rank = self.K
+        self.pad_size = T - 1
+        super().__init__((self.K, rank, T), (rank, self.M - T + 1), rank)
 
-    Attributes
+    def reconstruct(self, H, W):
+        return F.conv1d(H[None, :], W.flip(2), padding=self.pad_size)[0]
 
-    H : Tensor, [n_components, n_samples]
-        Activation matrix.
-
-    W : Tensor, [n_features, n_compoments, n_timesteps]
-        Template matrix.
-
-    """
-
-    def __init__(self, Xshape, T=1, n_components=None):
-        super().__init__(Xshape, n_components)
-        self.T = T
-        self.W = torch.nn.Parameter(torch.rand(self.K, self.rank, self.T))
-        self.H = torch.nn.Parameter(torch.rand(self.rank, self.M - self.T + 1))
-
-    def forward(self, H=None, W=None):
-        if H is None:
-            H = self.H
-        if W is None:
-            W = self.W
-        return F.conv1d(H[None, :], W.flip(2), padding=self.T - 1)[0]
-
-    def _get_W_positive(self, WH, beta, H_sum=None):
+    def get_W_positive(self, WH, beta, H_sum):
         H = self.H
         if beta == 1:
             if H_sum is None:
@@ -218,12 +198,12 @@ class NMFD(NMF):
         else:
             if beta != 2:
                 WH = WH.pow(beta - 1)
-            WHHt = F.conv1d(H[:, None], WH[:, None], padding=self.T - 1)
-            denominator = WHHt.transpose(0, 1).flip(2)
+            WHHt = F.conv1d(WH[:, None], H[:, None])
+            denominator = WHHt
 
         return denominator, H_sum
 
-    def _get_H_positive(self, WH, beta, W_sum=None):
+    def _get_H_positive(self, WH, beta, W_sum):
         W = self.W
         if beta == 1:
             if W_sum is None:
