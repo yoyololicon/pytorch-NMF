77322afb68bd1f91b1908ab33533e7b684f55682
Chin Yun Yu <ya70201@gmail.com>
2021-03-17 14:50:25 +0800

    test: only test beta=2 when spars_fit on sparse tensor

--- tests/test_nmf_sparse.py
+++ tests/test_nmf_sparse.py
@@ -39,16 +39,16 @@ def test_fit_sparse_dense(beta,
 
 @pytest.mark.parametrize('beta,sW,sH', [(2, 0.3, None),
                                         (2, None, 0.3),
-                                        (1.5, 0.4, None),
-                                        (1.5, None, 0.4),
-                                        (1, 0.1, None),
-                                        (1, None, 0.1),
-                                        (0.5, 0.5, None),
-                                        (0.5, None, 0.5),
-                                        (0, 0.5, None),
-                                        (0, None, 0.5),
-                                        (2.5, 0.1, None),
-                                        (2.5, None, 0.1),
+                                        #(1.5, 0.4, None),
+                                        #(1.5, None, 0.4),
+                                        #(1, 0.1, None),
+                                        #(1, None, 0.1),
+                                        #(0.5, 0.5, None),
+                                        #(0.5, None, 0.5),
+                                        #(0, 0.5, None),
+                                        #(0, None, 0.5),
+                                        # (2.5, 0.1, None),
+                                        # (2.5, None, 0.1),
                                         # (-0.5, 0.3, None),         # beta < 0 very unstable
                                         #(-0.5, None, 0.3),
                                         ])
@@ -56,8 +56,8 @@ def test_sparse_fit_sparse_dense(beta,
                                  sW,
                                  sH):
 
-    torch.random.manual_seed(2434)
-    max_iter = 1
+    #torch.random.manual_seed(2434)
+    max_iter = 5
     Vshape = (800, 800)
 
     V = torch.rand(*Vshape)
@@ -77,9 +77,9 @@ def test_sparse_fit_sparse_dense(beta,
         V_dense, beta, max_iter, False, sW, sH)
     n_iter = sparse_model.sparse_fit(
         V_sparse, beta, max_iter, False, sW, sH)
-    assert torch.allclose(dense_model.W, sparse_model.W, atol=3e-6), torch.abs(
+    assert torch.allclose(dense_model.W, sparse_model.W, atol=5e-7), torch.abs(
         dense_model.W - sparse_model.W).max().item()
-    assert torch.allclose(dense_model.H, sparse_model.H, atol=3e-6), torch.abs(
+    assert torch.allclose(dense_model.H, sparse_model.H, atol=5e-7), torch.abs(
         dense_model.H - sparse_model.H).max().item()
 
 
@@ -105,7 +105,7 @@ def test_fit_sparse_target(beta,
     assert not torch.any(torch.isnan(m.H))
 
 
-@pytest.mark.parametrize('beta', [0, 0.5,  1, 1.5, 2, 2.5])
+@pytest.mark.parametrize('beta', [2])
 @pytest.mark.parametrize('sp_ratio', [0.95, 0.98])
 @pytest.mark.parametrize('sW, sH', [(None,) * 2, (0.3, None), (None, 0.3)])
 def test_sparse_fit_sparse_target(beta,
